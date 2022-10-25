612a422eae7c3772bdc2cc0685ffdbe828ddfa3e
Sagar Raval <62638370+SRaval20@users.noreply.github.com>
2022-10-24 17:47:19 -0700

    Update stock-calculator.py

--- stock-calculator.py
+++ stock-calculator.py
@@ -53,5 +53,5 @@ def main():
         return render_template("index.html", **stock_data)
 
 if __name__ == '__main__':
-    app = create_app()
+    #app = create_app()
     app.run(debug=False, host='0.0.0.0')
